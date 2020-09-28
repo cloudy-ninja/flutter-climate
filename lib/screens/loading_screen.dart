82179cb12fdfdf53b567d9e4d5525cfd0c3c20a5
cloudy-ninja <msr0327@outlook.com>
2020-09-28 22:25:07 +0800

    fix url typo add LocationScreen in the LoadingScreen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
 
 const apiKey = 'b1b41feadf39a18db8181ee9c6e260d7';
 
@@ -20,17 +21,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getGeolocationData() async {
+    print('ok');
     Location location = Location();
     await location.getLocation();
     latitude = location.latitude;
     longitude = location.longitude;
 
-    NetworkingHelper networking = NetworkingHelper('api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    NetworkingHelper networking = NetworkingHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networking.getData();
+    print(weatherData['main']['temp']);
 
-    // double temperarture = weatherData['main']['temp'];
+    // double temperature = weatherData['main']['temp'];
     // int condition = weatherData['weather'][0]['id'];
     // String cityName = weatherData['name'];
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   // void getData() async {
